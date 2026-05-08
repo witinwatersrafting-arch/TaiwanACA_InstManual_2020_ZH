904a6be83e5374c044c71f03e251875af2b38cc9
witin <witin.waters.rafting@gmail.com>
2026-05-08 21:53:19 +0800

    feat: add and configure tailwindcss integration in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://witinwatersrafting-arch.github.io',
   base: '/TaiwanACA_InstManual_2020_ZH',
-  integrations: [],
+  integrations: [tailwind()],
 });
